82ee83bf2f9b3b880eeb13e30fd3862eef403863
ML <moti.lavian@chargeafter.com>
2017-08-16 01:39:33 +0300

    index

---
+++ index_files/floating-box.js
@@ -0,0 +1,30 @@
+﻿
+function moveScroller() {
+    var move = function () {
+        
+            var st = $(window).scrollTop();
+            var ot = $(".42-jq-floating-box-position").offset().top;
+            var s = $(".42-jq-floating-box");
+            if (st > ot && photoObj.isMobile() == false) {
+                s.css({
+                    position: "fixed",
+                    top: "0px",
+                    width: 'inherit'
+                });
+            } else {
+                if (st <= ot) {
+                    s.css({
+                        position: "relative",
+                        top: ""
+                    });
+                }
+            }
+        
+    };
+    $(window).scroll(move);
+    move();
+}
+
+$(function() {
+    moveScroller();
+});
---
+++ index_files/required-asterik.js
@@ -0,0 +1,29 @@
+﻿/* THIS SCRIPT TO ADD REQUIRED ASTERIK ON MVC INPUTS AND TEXTAREAS */
+
+$(function() {
+    $('form').each(function() {
+        var $form = $(this);
+        
+        var elements = $(this).find('input[type=text],input[type=password], textarea, select');
+
+        elements.each(function() {
+
+            // check if element has required attribute.
+            var req = $(this).attr('data-val-required') || $(this).data('required');
+
+            if (undefined != req) { //if required
+                //since MVC add for attribute to each label with same id of input. 
+                //we will get that label and will add asterik to label.
+                var label = $form.find('label[for="' + $(this).attr('id') + '"]');
+
+                var text = label.text();
+                //if label has text then add span with red colored asterik
+                if (text.length > 0) {
+                    label.append('<span style="color:red"> *</span>');
+                }
+            }
+        });
+    });
+});
+  
+
